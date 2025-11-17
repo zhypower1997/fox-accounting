49775794ad4561792a492508edfd48460bd3849c
zhypower1997 <854091383@qq.com>
2025-11-17 19:15:21 +0800

    feat: 增加icon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable standalone output for Docker runtime
+  output: "export",
 };
 
 export default nextConfig;
